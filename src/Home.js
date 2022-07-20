d4c5ee3bea78a88dd5a3ec5f5123a664ddbceacc
Akinbinu Arike <akinbinuarike2020@gmail.com>
2022-07-19 23:02:21 -0700

    first commit

--- src/Home.js
+++ src/Home.js
@@ -1,28 +1,11 @@
 import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const[error, setError] = useState(null);
-  useEffect(() => {
-    setTimeout(() => {
-      fetch(" http://localhost:8000/blogsr")
-        .then((res) => {
-          if (!res.ok) {
-            throw Error('could not fetch the data for that resource')
-          }
+  
+const { data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs')
 
-          return res.json();
-        })
-        .then((data) => {
-          setBlogs(data);
-          setIsPending(false);
-        })
-        .catch((err) => {
-setError(err.message);
-        });
-    }, 1000);
-  }, []);
+
 
   return (
     <div className="home">
